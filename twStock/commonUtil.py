42824efb3a4727df25378a93e0b8b1a506f3fffd
Benson <bensonchen@cht.com.tw>
2018-08-06 17:28:08 +0800

    benson 0806 common Util

--- twStock/commonUtil.py
+++ twStock/commonUtil.py
@@ -1,2 +1,26 @@
-def testfun(x, y):
-    return x + y
+from sklearn import preprocessing
+
+
+def prepare_data(df, test_data_rate):
+    print(len(df.index))
+    test_data_size = int(len(df.index) * test_data_rate)
+
+    x_train, x_test = df[0:test_data_size], df[test_data_size:]
+    print(x_train.shape)
+    print(x_test.shape)
+
+    # x_train = df[:, 0:test_data_size]
+    print("test_data_size", test_data_size, df.shape)
+    print(x_train.tail())
+    print(x_test.head())
+    return x_train, x_test
+
+
+def normalize_data(df):
+    min_max_scaler = preprocessing.MinMaxScaler()
+    df['open'] = min_max_scaler.fit_transform(df.open.values.reshape(-1, 1))
+    df['high'] = min_max_scaler.fit_transform(df.high.values.reshape(-1, 1))
+    df['low'] = min_max_scaler.fit_transform(df.low.values.reshape(-1, 1))
+    df['volume'] = min_max_scaler.fit_transform(df.volume.values.reshape(-1, 1))
+    df['fluctuation'] = min_max_scaler.fit_transform(df['fluctuation'].values.reshape(-1, 1))
+    return df
--- twStock/txLSTM1.py
+++ twStock/txLSTM1.py
@@ -1,5 +1,16 @@
 #from twStock.commonUtil import testfun
-import twStock.commonUtil
+#import twStock.commonUtil
+import pandas as pd
 
-r = twStock.testfun(1, 1)
-print(r)
+from twStock import commonUtil
+#讀csv進來變成dataframe格式
+df = pd.read_csv("./data/tx/2017tx.csv", encoding="utf-8")
+#轉換df所需欄位為-1到1的格式，最佳化時計算所需
+df = commonUtil.normalize_data(df)
+
+#訓練資料比例70%、測試資料30%
+test_data_rate = 0.7
+x_train, x_test = commonUtil.prepare_data(df, test_data_rate)
+
+
+
