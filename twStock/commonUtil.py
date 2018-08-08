41793b2c0c722075706457ab9a8dd6b3387e53df
Benson <bensonchen@cht.com.tw>
2018-08-08 17:27:35 +0800

    first twstock LSTM

--- twStock/commonUtil.py
+++ twStock/commonUtil.py
@@ -1,4 +1,6 @@
 from sklearn import preprocessing
+import numpy as np
+import pandas as pd
 
 
 def prepare_data(df, test_data_rate):
@@ -23,4 +25,32 @@ def normalize_data(df):
     df['low'] = min_max_scaler.fit_transform(df.low.values.reshape(-1, 1))
     df['volume'] = min_max_scaler.fit_transform(df.volume.values.reshape(-1, 1))
     df['fluctuation'] = min_max_scaler.fit_transform(df['fluctuation'].values.reshape(-1, 1))
-    return df
+    df_dict = {
+                "open": df['open'],
+                "high": df['high'],
+                "low": df['low'],
+                "volume": df['volume'],
+                "fluctuation": df['fluctuation']
+    }
+    dframe = pd.DataFrame(df_dict)
+    return dframe
+
+
+# Build Training Data
+# 輸入X_train: 利用前30天的Open, High, Low, Close, Adj Close, Volume, month, year, date, day作為Features，shape為(30, 10)
+# 輸出Y_train: 利用未來5天的Adj Close作為Features，shape為(5,1)
+def build_train(train, pastDay=30, futureDay=5):
+    x_train, y_train = [], []
+    for i in range(train.shape[0] - futureDay - pastDay):
+        x_train.append(np.array(train.iloc[i:i + pastDay]))
+        y_train.append(np.array(train.iloc[i + pastDay:i + pastDay + futureDay]["fluctuation"]))
+    return np.array(x_train), np.array(y_train)
+
+
+# 將Training Data取一部份當作Validation Data
+def split_data(X, Y, rate):
+    X_train = X[int(X.shape[0] * rate):]
+    Y_train = Y[int(Y.shape[0] * rate):]
+    X_val = X[:int(X.shape[0] * rate)]
+    Y_val = Y[:int(Y.shape[0] * rate)]
+    return X_train, Y_train, X_val, Y_val
--- twStock/txLSTM1.py
+++ twStock/txLSTM1.py
@@ -1,16 +1,57 @@
-#from twStock.commonUtil import testfun
-#import twStock.commonUtil
+# from twStock.commonUtil import testfun
+# import twStock.commonUtil
 import pandas as pd
-
+import numpy as np
 from twStock import commonUtil
-#讀csv進來變成dataframe格式
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, LSTM, TimeDistributed, RepeatVector
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import matplotlib.pyplot as plt
+
+
+# %matplotlib inline
+
+
+def buildOneToOneModel(shape):
+    print("shape", shape[1], shape[2])
+    model = Sequential()
+    model.add(LSTM(10, input_shape=(shape[1], shape[2])))
+    # output shape: (1, 1)
+    # model.add(TimeDistributed(Dense(3)))
+    # or use model.add(Dense(1))
+    model.add(Dense(1))
+    model.compile(loss="mean_squared_error", optimizer="adam")
+    model.summary()
+    return model
+
+
+# 讀csv進來變成dataframe格式
+df = pd.DataFrame()
 df = pd.read_csv("./data/tx/2017tx.csv", encoding="utf-8")
-#轉換df所需欄位為-1到1的格式，最佳化時計算所需
+# 轉換df所需欄位為-1到1的格式，最佳化時計算所需
 df = commonUtil.normalize_data(df)
+df = df.replace("-", np.nan)
+print(df.tail())
+# 訓練資料比例70%、測試資料30%
+# test_data_rate = 0.7
+# x_train, x_test = commonUtil.prepare_data(df, test_data_rate)
+# build Data, use last 30 days to predict next 5 days
+X_train, Y_train = commonUtil.build_train(df, 1, 1)
 
-#訓練資料比例70%、測試資料30%
-test_data_rate = 0.7
-x_train, x_test = commonUtil.prepare_data(df, test_data_rate)
+# split training data and validation data
+X_train, Y_train, X_val, Y_val = commonUtil.split_data(X_train, Y_train, 0.2)
+print(X_train.shape)
 
+model = buildOneToOneModel(X_train.shape)
+# callback = EarlyStopping(monitor="loss", patience=30, verbose=1, mode="auto")
+# model.fit(X_train, Y_train, epochs=100, batch_size=1, validation_data=(X_val, Y_val), callbacks=[callback])
+model.fit(X_train, Y_train, epochs=100, batch_size=1, validation_data=(X_val, Y_val))
 
-
+# 預測
+# trainPredict = model.predict(X_train)
+# testPredict = model.predict(X_val)
+#
+# print("predict", trainPredict)
+# print("predict222", testPredict)
