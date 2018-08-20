0c82ba9ffa6fc9582a25f24cea30f51c0451a5a4
Benson <bensonchen@cht.com.tw>
2018-08-20 16:44:11 +0800

    add 2016 tx data

--- twStock/saveData.py
+++ twStock/saveData.py
@@ -1,14 +1,17 @@
 import pandas as pd
-
 import matplotlib.pyplot as plt
 from datetime import datetime
 import matplotlib.dates as mdates
+
+
+
+# ------------用name.csv的英文名稱取代中文名稱(第一列)----------------------
 
 # 交易日期	契約	到期月份(週別)	開盤價	最高價	最低價	收盤價	漲跌價	漲跌%	成交量	結算價	未沖銷契約數	最後最佳買價	最後最佳賣價	歷史最高價	歷史最低價
 # date	contract	contract_m	open	high	low	close	fluctuation	fluctuation_percent	volume	settlement	open_interest	last_best_buy	last_bset_sell	h_high	h_low
 from matplotlib.ticker import AutoLocator
 
-df = pd.read_csv("./data/2017_fut.csv", encoding="Big5",
+df = pd.read_csv("./data/2016_fut.csv", encoding="Big5",
                  dtype={"settlement": object, "open_interest": object})
 
 # df.rename(columns={"契約":'date'}, inplace=True)
@@ -19,7 +22,7 @@ tx = df.loc[df["contract"] == "TX"]
 tx = tx.sort_values(by=['date', 'contract_m'])
 tx = tx.groupby("date").head(1)
 
-tx.to_csv("./data/tx/2017tx.csv", encoding="utf-8", index=False)
+tx.to_csv("./data/tx/2016tx.csv", encoding="utf-8", index=False)
 
 
 #xs = [datetime.strptime(d, '%m/%d/%Y').date() for d in tx["date"]]
